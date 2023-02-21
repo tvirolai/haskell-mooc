202467f45d1ecd8b8b9cc63a8c201cf58e9952b5
tvirolai <tuomo.virolainen@siili.com>
2023-02-21 09:28:09 +0200

    Set 6 4-5

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -71,7 +71,9 @@ data List a = Empty | LNode a (List a)
   deriving Show
 
 instance Eq a => Eq (List a) where
-  (==) = todo
+  (==) Empty Empty = True
+  (==) (LNode a b) (LNode c d) = (a == c) && (b == d)
+  (==) _ _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 5: below you'll find two datatypes, Egg and Milk. Implement a
@@ -91,6 +93,15 @@ data Egg = ChickenEgg | ChocolateEgg
 data Milk = Milk Int -- amount in litres
   deriving Show
 
+class Price a where
+  price :: a -> Int
+
+instance Price Egg where
+ price ChickenEgg = 20
+ price ChocolateEgg = 30
+
+instance Price Milk where
+  price (Milk a) = 15 * a
 
 ------------------------------------------------------------------------------
 -- Ex 6: define the necessary instance hierarchy in order to be able
