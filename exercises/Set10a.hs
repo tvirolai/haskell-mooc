3c0e86a0315680c7ecef560efccce45cc3cce3dc
tvirolai <tuomo.virolainen@siili.com>
2024-12-17 20:31:05 +0200

    1-3 done from Set10a

--- exercises/Set10a.hs
+++ exercises/Set10a.hs
@@ -16,7 +16,8 @@ import Mooc.Todo
 --   take 10 (doublify [0..])  ==>  [0,0,1,1,2,2,3,3,4,4]
 
 doublify :: [a] -> [a]
-doublify = todo
+doublify [] = []
+doublify (x:xs) = x:[x] ++ doublify xs
 
 ------------------------------------------------------------------------------
 -- Ex 2: Implement the function interleave that takes two lists and
@@ -37,7 +38,10 @@ doublify = todo
 --   take 10 (interleave [1..] (repeat 0)) ==> [1,0,2,0,3,0,4,0,5,0]
 
 interleave :: [a] -> [a] -> [a]
-interleave = todo
+interleave [] [] = []
+interleave (a:ax) [] = a:[] ++ interleave ax []
+interleave [] (b:bx) = b:[] ++ interleave [] bx
+interleave (a:ax) (b:bx) = a:[b] ++ interleave ax bx
 
 ------------------------------------------------------------------------------
 -- Ex 3: Deal out cards. Given a list of players (strings), and a list
@@ -56,7 +60,7 @@ interleave = todo
 -- Hint: remember the functions cycle and zip?
 
 deal :: [String] -> [String] -> [(String,String)]
-deal = todo
+deal players cards = zip cards (cycle players)
 
 ------------------------------------------------------------------------------
 -- Ex 4: Compute a running average. Go through a list of Doubles and
