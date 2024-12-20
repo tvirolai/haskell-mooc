a44cef719ed8a9de38eac5066821a4b12cb052f7
tvirolai <tuomo.virolainen@siili.com>
2024-12-20 10:24:37 +0200

    Set10b done

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,7 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+x ||| y = if y then True else x
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +36,9 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength [] = 0
+boolLength (x:[]) = 1
+boolLength (x:xs) = if x then 1 + boolLength xs else 1 + boolLength xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +52,8 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value = case predicate value of
+  res -> if res then value else value
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +87,14 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq a b = if a then b else b
 
 instance MySeq Int where
-  myseq = todo
+  myseq a b = case a of
+    1 -> b
+    _ -> b
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] x = x
+  myseq (a:b) x = case a of
+    b -> x
