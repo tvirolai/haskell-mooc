15e9a570a5ffd3ed74a15ca7727d27152ebb651c
tvirolai <tuomo.virolainen@siili.com>
2024-12-21 21:48:57 +0200

    Set11a ex 1-6

--- exercises/Set11a.hs
+++ exercises/Set11a.hs
@@ -25,14 +25,19 @@ import Mooc.Todo
 -- first line should be HELLO and the second one WORLD
 
 hello :: IO ()
-hello = todo
+hello = do
+          putStrLn "HELLO"
+          putStrLn "WORLD"
 
 ------------------------------------------------------------------------------
 -- Ex 2: define the IO operation greet that takes a name as an
 -- argument and prints a line "HELLO name".
 
 greet :: String -> IO ()
-greet name = todo
+greet name =
+  do
+    putStr "HELLO "
+    putStrLn name
 
 ------------------------------------------------------------------------------
 -- Ex 3: define the IO operation greet2 that reads a name from the
@@ -42,7 +47,10 @@ greet name = todo
 -- Try to use the greet operation in your solution.
 
 greet2 :: IO ()
-greet2 = todo
+greet2 =
+  do
+    name <- getLine
+    greet name
 
 ------------------------------------------------------------------------------
 -- Ex 4: define the IO operation readWords n which reads n lines from
@@ -56,7 +64,12 @@ greet2 = todo
 --   ["alice","bob","carl"]
 
 readWords :: Int -> IO [String]
-readWords n = todo
+readWords 0 = return []
+readWords n =
+  do
+    answ <- getLine
+    rst <- readWords (n-1)
+    return $ sort (answ:rst)
 
 ------------------------------------------------------------------------------
 -- Ex 5: define the IO operation readUntil f, which reads lines from
@@ -73,13 +86,24 @@ readWords n = todo
 --   ["bananas","garlic","pakchoi"]
 
 readUntil :: (String -> Bool) -> IO [String]
-readUntil f = todo
+readUntil f =
+  do
+    answ <- getLine
+    if (f answ) then return []
+      else
+      do
+        rest <- readUntil f
+        return (answ:rest)
 
 ------------------------------------------------------------------------------
 -- Ex 6: given n, print the numbers from n to 0, one per line
 
 countdownPrint :: Int -> IO ()
-countdownPrint n = todo
+countdownPrint (-1) = return ()
+countdownPrint n =
+  do
+    print n
+    countdownPrint (n-1)
 
 ------------------------------------------------------------------------------
 -- Ex 7: isums n should read n numbers from the user (one per line) and
